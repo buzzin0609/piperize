34fa4d53bc0b7fdef45ae74b280422b0c7605785
Will Busby <will0609@hotmail.com>
2018-05-22 07:38:33 +0100

    Version 0.0.4 - organised exports

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var piperize_1 = require("./src/piperize");
+var pipeAsync_1 = require("./src/pipeAsync");
+exports.pipeAsync = pipeAsync_1["default"];
 exports["default"] = piperize_1["default"];
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 import piperize from "./src/piperize";
+import _pipeAsync from "./src/pipeAsync";
+
+export const pipeAsync = _pipeAsync;
 export default piperize;
