aafa6f3810fad631b0964f3319583b6d62700e71
william.busby <busbybmw69>
2018-05-29 15:16:12 +0100

    Version 0.0.8 - added 'this' bind target for pipeAsync

--- src/pipeAsync.ts
+++ src/pipeAsync.ts
@@ -1,14 +1,16 @@
+
+import onlyPure from "./onlyPure";
 /**
  * Pipes a list of callbacks asynchronously. The list can be a mix of async and regular callbacks
  * @param callbacks
  */
-import onlyPure from "./onlyPure";
-
 export default function pipeAsync(...callbacks: Function[]) {
     return async function piperizeAsyncPipeline(...inputs: any[]) {
-        const first = await callbacks[0](...inputs);
+        //use the call function so callers of this function can bind the this context properly
+        const first = await callbacks[0].call(this, ...inputs);
 
-        return piperizeAsyncReducer(
+        return piperizeAsyncReducer.call(
+            this,
             onlyPure(first, callbacks[0]),
             callbacks.slice(1)
         );
@@ -17,8 +19,9 @@ export default function pipeAsync(...callbacks: Function[]) {
 
 async function piperizeAsyncReducer(value: any, otherCallbacks: Function[]) {
     if (otherCallbacks.length) {
-        const newValue = await otherCallbacks[0](value);
-        return piperizeAsyncReducer(
+        const newValue = await otherCallbacks[0].call(this, value);
+        return piperizeAsyncReducer.call(
+            this,
             onlyPure(newValue, otherCallbacks[0]),
             otherCallbacks.slice(1)
         );
