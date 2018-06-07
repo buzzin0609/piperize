1b395f5b666c3f0abfb80f5f27d71de494c69640
Will Busby <will0609@hotmail.com>
2018-06-07 22:34:36 +0100

    Version 0.1.2 - Added ifElse curry function

---
+++ src/util/ifElse.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+function ifElse(trueValue, falseValue) {
+    return function ifElseResolver(value) {
+        if (!!value)
+            return trueValue;
+        return falseValue;
+    };
+}
+exports["default"] = ifElse;
+//# sourceMappingURL=ifElse.js.map
---
+++ src/util/ifElse.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * Test suite for ifElse module
+ */
+exports.__esModule = true;
+var ifElse_1 = require("./ifElse");
+describe('ifElse:', function () {
+    var tester = ifElse_1["default"]('foo', 'bar');
+    it('should return the trueValue if value to callback is truthy', function () {
+        expect(tester('test')).toEqual('foo');
+        expect(tester(1)).toEqual('foo');
+        expect(tester(true)).toEqual('foo');
+    });
+    it('should return the falseValue if value to callback is falsy', function () {
+        expect(tester('')).toEqual('bar');
+        expect(tester(0)).toEqual('bar');
+        expect(tester(false)).toEqual('bar');
+        expect(tester(undefined)).toEqual('bar');
+    });
+});
+//# sourceMappingURL=ifElse.spec.js.map
